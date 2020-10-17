8fb0bd66883fb8c4f1127c2a6851329baeb55104
andrew-cao <72300439+andrew-cao@users.noreply.github.com>
2020-10-17 23:41:54 +1100

    Update js_file.js

--- js/js_file.js
+++ js/js_file.js
@@ -1,5 +1,21 @@
 function openChart(evt, name) {
-  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(name).style.display = "block";
+  evt.currentTarget.className += " active";
+} 
+
+function openMap(evt, name) {
   var i, tabcontent, tablinks;
 
   tabcontent = document.getElementsByClassName("tabcontent");
