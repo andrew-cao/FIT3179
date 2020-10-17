ac675b4ed1e065a102f188563d8837993c32767b
andrew-cao <72300439+andrew-cao@users.noreply.github.com>
2020-10-18 00:04:53 +1100

    Update js_file.js

--- js/js_file.js
+++ js/js_file.js
@@ -1,4 +1,4 @@
-function openChart(evt, name) {
+function openMap(evt, name) {
   var i, tabcontent, tablinks;
 
   tabcontent = document.getElementsByClassName("tabcontent");
@@ -15,17 +15,17 @@ function openChart(evt, name) {
   evt.currentTarget.className += " active";
 } 
 
-function openMap(evt, name) {
-  var i, tabcontent, tablinks;
+function openChart(evt, name) {
+  var j, tabcontent2, tablinks2;
 
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+  tabcontent2 = document.getElementsByClassName("tabcontent");
+  for (j = 0; j < tabcontent2.length; j++) {
+    tabcontent2[j].style.display = "none";
   }
 
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  tablinks2 = document.getElementsByClassName("tablinks2");
+  for (j = 0; j < tablinks2.length; j++) {
+    tablinks2[j].className = tablinks2[j].className.replace(" active", "");
   }
 
   document.getElementById(name).style.display = "block";
